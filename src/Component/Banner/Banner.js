97d59ceea380666c29792b2bb149ed582ef321c8
Shubham Pawar <142664082+shubham-pawar9@users.noreply.github.com>
2026-02-28 21:00:17 +0530

    Revamp event booking UI with blocked-date calendar

--- src/Component/Banner/Banner.js
+++ src/Component/Banner/Banner.js
@@ -1,4 +1,4 @@
-import { Box, Typography, Grid } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const Styles = {
   menusBox: {
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { AppBar, Toolbar, Typography, IconButton, Box } from "@mui/material";
 
 const Styles = {
